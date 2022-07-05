4395f4985088893fa3d82fb63c77780407e427d0
Gabriel R Pacheco <gabrielrodriguespacheco2@gmail.com>
2022-07-05 19:32:48 -0300

    Montagem da funcao de criptografar e decriptografar

--- main.py
+++ main.py
@@ -3,16 +3,22 @@
 # Alunos:
 # * Gabriel Rodrigues Pacheco - 17/0058280
 # * Theo
+# TODO: Colocar o nome e matricula do Theo
 
 import re
 
 
-# Funcao chamada ao se selecionar a funcao de criptografar. Ela e responsavel por solicitar e validar os dados
-def lista_criptografar():
+# Funcao chamada ao se selecionar a funcao de criptografar (modo 1) ou decriptografar (modo 2). Ela e responsavel por
+# solicitar e validar os dados
+# Recebe um int como entrada que indica o modo de operacao
+def lista_criptografar(modo):
     msg = ""
     key = ""
     while msg == "":
-        msg = input("Qual mensagem deve ser criptografada?: ")
+        if modo == 1:
+            msg = input("Qual mensagem deve ser criptografada?: ")
+        else:
+            msg = input("Qual mensagem deve ser decriptografada?: ")
         msg = msg.upper()
         regex = re.compile('[^A-Z ]')
         msg = regex.sub('', msg)
@@ -21,13 +27,44 @@ def lista_criptografar():
         key = key.upper()
         regex = re.compile('[^A-Z]')
         key = regex.sub('', key)
-    print(msg)
-    print(key)
+    if modo == 1:
+        criptografar(msg, key, 1)
+    else:
+        criptografar(msg, key, 2)
 
 
-def lista_descriptografar():
-    # TODO: Criar funcao de descriptografar
-    print("Oi, ainda em construção")
+# Funcao que realiza a criptografia. Modo 1 significa criotografar e modo 2 descriptografar
+# Recebe duas strings de entrada, correspondentes a mensagem e a senha, e um int referente ao modo de operacao
+def criptografar(msg, key, modo):
+    count_key = 0
+    resultado = ''
+    # Percorre letra por letra realizando a cifracao ou decifracao e ignora os espacos
+    for letra in msg:
+        if letra == ' ':
+            # Se a letra for um espaco, ela e apenas copiada
+            letra_crip = ord(letra)
+        else:
+            # Realiza a soma entre a letra da msg e da chave
+            if modo == 1:
+                letra_crip = ord(letra) + (ord(key[count_key]) - ord('A'))
+            else:
+                letra_crip = ord(letra) - (ord(key[count_key]) - ord('A'))
+            # Se sair dos limites desejados, ele regride ao alfabeto
+            if letra_crip > ord('Z'):
+                letra_crip -= 26
+            if letra_crip < ord('A'):
+                letra_crip += 26
+        # Percorre a chave ate seu fim, ao chegar ao fim recomeca
+        count_key += 1
+        if count_key == len(key):
+            count_key = 0
+        # Adiciona o resultado a string de saida
+        resultado += chr(letra_crip)
+    if modo == 1:
+        print("Criptograma: ")
+    else:
+        print("Mensagem: ")
+    print(resultado)
 
 
 def lista_quebrar():
@@ -46,8 +83,8 @@ if __name__ == '__main__':
     while resposta != '1' and resposta != '2' and resposta != '3':
         resposta = input("Escolha: ")
     if resposta == '1':
-        lista_criptografar()
+        lista_criptografar(1)
     elif resposta == '2':
-        pass
+        lista_criptografar(2)
     else:
         pass
